f1b209bdcc73fcaabec5fab46c8a972bc43bdf07
maxime c <macol1304@gmail.com>
2023-04-04 16:50:55 -0400

    route films et films/id

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/Films", "App\Http\Controllers\FilmController@index");
+Route::get("/Films/{id}", "App\Http\Controllers\FilmController@show");
